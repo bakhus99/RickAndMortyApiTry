a5659a9b82d863bf3cc08a276ddb54f905543461
baha <tytenov99@gmail.com>
2021-04-14 18:03:45 +0600

    final change#6

--- app/src/main/java/com/bakhus/rickandmortyapitry/ui/main/adapter/LocationAdapter.kt
+++ app/src/main/java/com/bakhus/rickandmortyapitry/ui/main/adapter/LocationAdapter.kt
@@ -2,11 +2,9 @@ package com.bakhus.rickandmortyapitry.ui.main.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bakhus.rickandmortyapitry.databinding.LocationItemBinding
 import com.bakhus.rickandmortyapitry.models.LocationData
-import com.bakhus.rickandmortyapitry.ui.main.view.LocationsFragmentDirections
 
 class LocationAdapter : RecyclerView.Adapter<LocationAdapter.LocationsViewHolder>() {
 
@@ -31,13 +29,6 @@ class LocationAdapter : RecyclerView.Adapter<LocationAdapter.LocationsViewHolder
 
     override fun onBindViewHolder(holder: LocationsViewHolder, position: Int) {
         holder.bind(locationsList[position])
-        holder.itemView.setOnClickListener {
-            val action =
-                LocationsFragmentDirections.actionLocationsFragmentToLocationDetailFragment(
-                    locationsList[position]
-                )
-            it.findNavController().navigate(action)
-        }
     }
 
     override fun getItemCount() = locationsList.size
